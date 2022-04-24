8b46d627ee7cd0b6626baf7b32aca9d01ae520d4
Bayram Akan <akancodes@gmail.com>
2022-04-24 10:32:12 +0300

    added colors, fonts and grid settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,25 +1,26 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       colors: {
-        'custom-blue': 'hsl(246, 80%, 60%)',
-        'light-red-work': 'hsl(15, 100%, 70%)',
-        'soft-blue-play': 'hsl(195, 74%, 62%)',
-        'light-red-study': 'hsl(348, 100%, 68%)',
-        'lime-green-exercise': 'hsl(145, 58%, 55%)',
-        'violet-social': 'hsl(264, 64%, 52%)',
-        'soft-orange': 'hsl(43, 84%, 65%)',
-        'very-dark-blue': 'hsl(226, 43%, 10%)',
-        'dark-blue': 'hsl(235, 46%, 20%)',
-        'desaturated-blue': 'hsl(235, 45%, 61%)',
-        'pale-blue': 'hsl(236, 100%, 87%)'
+        "custom-blue": "hsl(246, 80%, 60%)",
+        "light-red-work": "hsl(15, 100%, 70%)",
+        "soft-blue-play": "hsl(195, 74%, 62%)",
+        "light-red-study": "hsl(348, 100%, 68%)",
+        "lime-green-exercise": "hsl(145, 58%, 55%)",
+        "violet-social": "hsl(264, 64%, 52%)",
+        "soft-orange": "hsl(43, 84%, 65%)",
+        "very-dark-blue": "hsl(226, 43%, 10%)",
+        "dark-blue": "hsl(235, 46%, 20%)",
+        "desaturated-blue": "hsl(235, 45%, 61%)",
+        "pale-blue": "hsl(236, 100%, 87%)",
       },
       fontFamily: {
-        Rubik: ['Rubik', 'sans-serif']
-      }
+        Rubik: ["Rubik", "sans-serif"],
+      },
+      gridTemplateRows: {
+        7: "repeat(7, minmax(0, 1fr))",
+      },
     },
   },
   plugins: [],
